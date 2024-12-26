6b002fd6a2d184411c70c743942f4143f7e25ea2
palmoisii <kaito.mznf@gmail.com>
2024-12-26 17:26:45 +0900

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
-  site: "https://screwfast.uk",
+  site: "https://reserve.nf.la",
   image: {
     domains: ["images.unsplash.com"],
   },
